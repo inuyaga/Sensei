4f700bc3ddbcf71bc9d09a33d82284f6b14e268a
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2020-03-09 10:53:42 -0600

    config admin panel

--- aplicaciones/ctr_escolar/admin.py
+++ aplicaciones/ctr_escolar/admin.py
@@ -1,12 +1,53 @@
 from django.contrib import admin
-from aplicaciones.ctr_escolar.models import Aula, Blog, CalificacionMateria, CalificacionUnidad, ComentarioBlog, Documento, Materia, Tarea
+from aplicaciones.ctr_escolar.models import Aula, Blog, CalificacionMateria, CalificacionUnidad, ComentarioBlog, Documento, Materia, Tarea, TareaDocumento
 
 # Register your models here.
-admin.site.register(Aula)
-admin.site.register(Blog)
+class AulaConfig(admin.ModelAdmin):
+    list_display = (
+        'aula_nombre',
+        'aula_pertenece',
+        'aula_creacion',
+    )
+    raw_id_fields = ['aula_pertenece']
+    search_fields = (
+        'aula_nombre',
+        'aula_pertenece',
+        )
+class BlogConfig(admin.ModelAdmin):
+    list_display = (
+        'blog_titulo',
+        'blog_descripcion',
+        'blog_pertenece',
+        'blog_materia',
+    )
+    raw_id_fields = ['blog_pertenece', 'blog_materia']
+    search_fields = (
+        'blog_pertenece',
+        'blog_materia',
+        )
+
+class TareaDocumentoConfig(admin.ModelAdmin):
+    list_display = (
+        'tareaDocumento_creado',
+        'tareaDocumento_pertenece',
+        'tareaDocumento_Tarea',
+        'tareaDocumento_status',
+        'tareaDocumento_calificacion',
+    )
+    raw_id_fields = ['tareaDocumento_pertenece', 'tareaDocumento_Tarea']
+    search_fields = (
+        'tareaDocumento_pertenece__username',
+        'tareaDocumento_Tarea__tarea_nombre',
+        )
+    list_filter = ['tareaDocumento_status', 'tareaDocumento_creado']
+
+
+admin.site.register(Aula, AulaConfig)
+admin.site.register(Blog, BlogConfig)
 admin.site.register(CalificacionMateria)
 admin.site.register(CalificacionUnidad)
 admin.site.register(ComentarioBlog)
 admin.site.register(Documento)
 admin.site.register(Materia)
-admin.site.register(Tarea)
+admin.site.register(Tarea) 
+admin.site.register(TareaDocumento, TareaDocumentoConfig) 
