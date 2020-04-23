02bf51f59c3b991c656878038120923efc108e1d
Mario Esteban in Windows <hdez.marioe@gmail.com>
2020-04-23 17:47:58 -0500

    pagina principal editada

--- aplicaciones/web/views.py
+++ aplicaciones/web/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from aplicaciones.ctr_escolar.models import Blog
 class IndexView(TemplateView):
     template_name = "inicio/inicio.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blogs']=Blog.objects.all().order_by('blog_creado')[:3]
+        return context
+    
 
 
 class LoginView(LoginView):
